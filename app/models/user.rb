4a250649648ec8888ff3dddbc363f5e10fa66aac
itokanta <80323505+itokanta@users.noreply.github.com>
2021-04-14 00:16:44 +0900

    バリデーションの追加設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :second_name, presence: true
-  validates :first_name, presence: true
-  validates :second_furigana, presence: true
-  validates :first_furigana, presence: true
+  validates :second_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :second_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: PASSWORD_REGEX}
 end
