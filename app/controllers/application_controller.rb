a8eb4edbec8420b339fee35c3149a78ab7ec4c8f
itokanta <80323505+itokanta@users.noreply.github.com>
2021-04-15 13:15:13 +0900

    Revert "ユーザー管理機能"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  private
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :second_name, :first_name, :second_furigana, :first_furigana, :birthday])
-  end
 end
