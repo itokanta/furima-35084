3ef8e0432142e77396926fc4987de98304d029d4
itokanta <80323505+itokanta@users.noreply.github.com>
2021-04-16 16:45:51 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,19 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
   belongs_to :postage
   belongs_to :from
   belongs_to :send_about
+  belongs_to :user
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id,   numericality: { other_than: 1 }
+  validates :description, presence: true
+  validates :postage_id,  numericality: { other_than: 1 }
+  validates :from_id,     numericality: { other_than: 1 }
+  validates :send_about_id, numericality: { other_than: 1 }
+  validates :price, presence: true
 end
