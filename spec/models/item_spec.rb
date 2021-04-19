58ad6a9f4f493173766e4e3d14f824e4b6860225
itokanta <80323505+itokanta@users.noreply.github.com>
2021-04-19 11:49:51 +0900

    テストコードの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Item, type: :model do
         expect(@item).to be_valid
       end
       it '販売価格が¥300~¥9,999,999の間であり、かつ半角数字のみで入力されていれば保存できる' do
-        @item.price = '10000'
+        @item.price = 10000
         expect(@item).to be_valid
       end
     end
@@ -31,30 +31,30 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
-      it 'category_idが空では保存できない' do
-        @item.category_id = ''
+      it 'category_idが「---」では保存できない' do
+        @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Category is not a number')
+        expect(@item.errors.full_messages).to include('Category must be other than 1')
       end
-      it 'status_idが空では保存できない' do
-        @item.status_id = ''
+      it 'status_idが「---」では保存できない' do
+        @item.status_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Status is not a number')
+        expect(@item.errors.full_messages).to include('Status must be other than 1')
       end
-      it 'postage_idが空では保存できない' do
-        @item.postage_id = ''
+      it 'postage_idが「---」では保存できない' do
+        @item.postage_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Postage is not a number')
+        expect(@item.errors.full_messages).to include('Postage must be other than 1')
       end
-      it 'from_idが空では保存できない' do
-        @item.from_id = ''
+      it 'from_idが「---」では保存できない' do
+        @item.from_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include('From is not a number')
+        expect(@item.errors.full_messages).to include('From must be other than 0')
       end
-      it 'sendabout_idが空では保存できない' do
-        @item.sendabout_id = ''
+      it 'sendabout_idが「---」では保存できない' do
+        @item.sendabout_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Sendabout is not a number')
+        expect(@item.errors.full_messages).to include('Sendabout must be other than 1')
       end
       it 'priceが空では保存できない' do
         @item.price = ''
@@ -62,17 +62,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it 'priceが300未満では保存できない' do
-        @item.price = '100'
+        @item.price = 100
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it 'priceが10,000,000以上では保存できない' do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
-      it 'priceが半角数字以外では保存できない' do
-        @item.price = 'あ１１１'
+      it 'priceが全角数字では保存できない' do
+        @item.price = '１１１１'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it 'priceが半角英数字混合では保存できない' do
+        @item.price = 'aa111'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it 'priceが半角英語では保存できない' do
+        @item.price = 'aaaa'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
