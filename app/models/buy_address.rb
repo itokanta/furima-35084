bc539e6aec8de75608c9c53c183357552935c1f5
itokanta <80323505+itokanta@users.noreply.github.com>
2021-04-24 16:23:43 +0900

    モデルの修正

--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -6,7 +6,7 @@ class BuyAddress
     validates :item_id
     validates :user_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :from_id, numericality: {other_than: 1, "can't be blank"}
+    validates :from_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :cities
     validates :address
     validates :phone, length: {maximum: 11}, format: {with: /\A[0-9]+\z/}
@@ -15,4 +15,5 @@ class BuyAddress
   def save
     buy = Buy.create(item_id: item_id, user_id: user_id)
     Address.create(postal_code: postal_code, from_id: from_id, cities: cities, address: address, build_name: build_name, phone: phone, buy_id: buy_id)
+  end
 end
