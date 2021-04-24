a8cb282bb37323067608ff4774e72e53fa6e4613
itokanta <80323505+itokanta@users.noreply.github.com>
2021-04-24 15:30:54 +0900

    カラム名の修正

--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -1,12 +1,12 @@
 class BuyAddress
   include ActiveModel::Model
-  attr_accessor :item, :user, :postal_code, :from, :cities, :address, :build_name, :phone
+  attr_accessor :item_id, :user_id, :postal_code, :from_id, :cities, :address, :build_name, :phone
 
   with_options presence: true do
     validates :item_id
     validates :user_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :from, numericality: {other_than: 1, "can't be blank"}
+    validates :from_id, numericality: {other_than: 1, "can't be blank"}
     validates :cities
     validates :address
     validates :phone, length: {maximum: 11}, format: {with: /\A[0-9]+\z/}
@@ -14,5 +14,5 @@ class BuyAddress
 
   def save
     buy = Buy.create(item_id: item_id, user_id: user_id)
-    Address.create(postal_code: postal_code, from: from, cities: cities, address: address, build_name: build_name, phone: phone, buy_id: buy_id)
+    Address.create(postal_code: postal_code, from_id: from_id, cities: cities, address: address, build_name: build_name, phone: phone, buy_id: buy_id)
 end
