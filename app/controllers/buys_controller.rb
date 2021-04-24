1bce18dde066a474f38c23dcde0c2b6a60e26d89
itokanta <80323505+itokanta@users.noreply.github.com>
2021-04-24 16:10:04 +0900

    コントローラーの編集

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -2,8 +2,17 @@ class BuysController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    # @buy_address = BuyAddress.new(buy_params)
-
+    @buy_address = BuyAddress.new(buy_params)
+    if @buy_address.valid?
+      buy_address.save
+      redirect_to root_path
+    else
+      render :index
   end
 
+  private
+  
+  def buy_params
+    params.require(:buy_address).permit(:postal_code, :from_id, :cities, :address, :build_name, :phone).merge(user_id: current_user.id, item_id: params[:id])
+  end
 end
