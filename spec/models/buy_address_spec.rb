4a01cae0938353930dd683390deb1f28afa6fa14
itokanta <80323505+itokanta@users.noreply.github.com>
2021-04-26 14:55:07 +0900

    テストコードの追記

--- spec/models/buy_address_spec.rb
+++ spec/models/buy_address_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe BuyAddress, type: :model do
       user = FactoryBot.create(:user)
       item = FactoryBot.create(:item)
       @buy_address = FactoryBot.build(:buy_address, user_id: user.id, item_id: item.id)
+      sleep(1) 
     end
 
     context '内容に問題ない場合' do
@@ -30,11 +31,40 @@ RSpec.describe BuyAddress, type: :model do
         @buy_address.valid?
         expect(@buy_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
       end
-      # it 'from_idが1だと保存できない' do
-      #   @buy_address.from_id = 1
-      #   @buy_address.valid?
-      #   expect(@buy_address.errors.full_messages).to include("From can't be blank")
-      # end
+      it 'from_idが1だと保存できない' do
+        @buy_address.from_id = 1
+        @buy_address.valid?
+        expect(@buy_address.errors.full_messages).to include("From can't be blank")
+      end
+      it 'cityが空だと保存できない' do
+        @buy_address.city = ''
+        @buy_address.valid?
+        expect(@buy_address.errors.full_messages).to include("City can't be blank")
+      end
+      it 'build_nameが空でも保存できる' do
+        @buy_address.build_name = ''
+        expect(@buy_address).to be_valid
+      end
+      it 'addressが空だと保存できない' do
+        @buy_address.address = ''
+        @buy_address.valid?
+        expect(@buy_address.errors.full_messages).to include("Address can't be blank")
+      end
+      it 'phoneが空だと保存できない' do
+        @buy_address.phone = ''
+        @buy_address.valid?
+        expect(@buy_address.errors.full_messages).to include("Phone can't be blank")
+      end
+      it 'phoneが11文字以上だと保存できない' do
+        @buy_address.phone = '1111111111111'
+        @buy_address.valid?
+        expect(@buy_address.errors.full_messages).to include("Phone is too long (maximum is 11 characters)")
+      end
+      it 'phoneが半角数字以外だと保存できない' do
+        @buy_address.phone = '１１１１１１１１１１１'
+        @buy_address.valid?
+        expect(@buy_address.errors.full_messages).to include("Phone is invalid")
+      end
     end
   end
 end
