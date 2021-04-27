1ec1de3a2fec96d372db5a2115f5c6f3a28633c8
itokanta <80323505+itokanta@users.noreply.github.com>
2021-04-27 16:25:45 +0900

    クレジットカード決済機能実装

--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -1,6 +1,6 @@
 class BuyAddress
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :postal_code, :from_id, :city, :address, :build_name, :phone
+  attr_accessor :item_id, :user_id, :postal_code, :from_id, :city, :address, :build_name, :phone, :token
 
   with_options presence: true do
     validates :item_id
@@ -10,10 +10,11 @@ class BuyAddress
     validates :city
     validates :address
     validates :phone, length: {maximum: 11}, format: {with: /\A[0-9]+\z/}
+    validates :token
   end
 
   def save
     buy = Buy.create(item_id: item_id, user_id: user_id)
-    Address.create(postal_code: postal_code, from_id: from_id, city: city, address: address, build_name: build_name, phone: phone, buy_id: buy_id)
+    Address.create(postal_code: postal_code, from_id: from_id, city: city, address: address, build_name: build_name, phone: phone, buy_id: buy.id)
   end
 end
